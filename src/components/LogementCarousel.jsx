ebdcffccf3fa86adae27869ad3d49692487c5fac
Elise Martinez <elise.martinezphn@gmail.com>
2023-04-28 09:52:48 +0200

    changement caroussel pas encore fonctionnel avec les modifications demandées

--- src/components/LogementCarousel.jsx
+++ src/components/LogementCarousel.jsx
@@ -14,13 +14,13 @@ const LogementCarousel = ({ logementPictures }) => {
     const imageNumber = logementPictures.length;
 
     // Au click sur une flèche nous changeons l'index dans le tableau l'image
-    const carouselDownScrolling = () => {
+    const carouselLeftScrolling = () => {
         if (currentIndex < 1) {
             return setCurrentIndex(imageNumber - 1)
         }
         return setCurrentIndex(currentIndex - 1)
     }
-    const carouselUpScrolling = () => {
+    const carouselRightScrolling = () => {
         if (currentIndex === logementPictures.length - 1) {
             return setCurrentIndex(0)
         }
@@ -28,38 +28,28 @@ const LogementCarousel = ({ logementPictures }) => {
     }
 
     // Si le nombre d'images est supérieur à 1 on affiche un carousel
-    if (imageNumber > 1) {
-        return (
-            <div className="carouselContainer">
-                <div className="carouselImg" style={{
-                    backgroundPositionY: "center",
-                    backgroundPositionX: "center",
-                    backgroundSize: "cover",
-                    backgroundImage: `url(${logementPictures[currentIndex]})`
-                }}>
-                    <div className="carouselSwitchArrows">
-                        <img onClick={carouselDownScrolling} className="arrowLeft" src={arrowLeft} alt="Photo précédente" />
-                        <img onClick={carouselUpScrolling} className="arrowRight" src={arrowRight} alt="Photo suivante" />
+    return (
+        <div className="carouselContainer">
+            <div className="carouselImg" style={{
+                backgroundPositionY: "center",
+                backgroundPositionX: "center",
+                backgroundSize: "cover",
+                backgroundImage: `url(${logementPictures[currentIndex]})`
+            }}>
+                {(imageNumber > 1) ??
+                    <div>
+                        <div className="carouselSwitchArrows">
+                            <img onClick={carouselLeftScrolling} className="arrowLeft" src={arrowLeft} alt="Photo précédente" />
+                            <img onClick={carouselRightScrolling} className="arrowRight" src={arrowRight} alt="Photo suivante" />
+                        </div>
+                        <p className="carouselCounter">{currentIndex + 1}/{logementPictures.length}</p>
                     </div>
-                    <p className="carouselCounter">{currentIndex + 1}/{logementPictures.length}</p>
-                </div>
+                }
             </div>
-        )
-    }
-    // Sinon on affiche seulement une image
-    else {
-        return (
-            <div className="carouselContainer">
-                <div className="carouselImg" style={{
-                    backgroundPositionY: "center",
-                    backgroundPositionX: "center",
-                    backgroundSize: "cover",
-                    backgroundImage: `url(${logementPictures[currentIndex]})`
-                }}>
-                </div>
-            </div>
-        )
-    }
+        </div>
+    )
+
+
 }
 
 export default LogementCarousel;
